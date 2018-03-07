e2b5f6bc795fd7b721ec44751176971d071d27fd
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-07 17:00:40 -0500

    added schema and seeds sql

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,8 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "burgers_db"
+    database: "beer_db"
 });
-
 
 connection.connect(function (err) {
     if (err) {
