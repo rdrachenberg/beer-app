27d9004063c7f8226832d37ac32306c3b579eeef
PlugInPlaya <tsanders1114@gmail.com>
2018-03-10 11:47:09 -0500

    Still Making Changes

---
+++ public/assets/js/beers.js
@@ -0,0 +1,18 @@
+<script type="text/javascript" src="assets/js"</script>
+<script type="text/javascript">
+$(document).ready(function(){
+    // Check Radio-box
+    $(".rating input:radio").attr("checked", false);
+
+    $('.rating input').click(function () {
+        $(".rating span").removeClass('checked');
+        $(this).parent().addClass('checked');
+    });
+
+    $('input:radio').change(
+      function(){
+        var userRating = this.value;
+        alert(userRating);
+    }); 
+});
+</script>
