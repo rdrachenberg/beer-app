4688b2f634298da6ef0beb87a5ccea77305b42e8
unknown <jkue3000@gmail.com>
2018-03-10 12:31:24 -0500

    MORE BEER JS

--- public/assets/js/beers.js
+++ public/assets/js/beers.js
@@ -1,34 +1,47 @@
+console.log("LOADED BEERS JS")
+
 var breweryDbApiKey = "27be3f3d0eb10fc0eec138e6cbb4b8f8";
-var userSearch = "tampa";
+
+// CANHGE TO USER SEARCH INPUT
+var userSearch = "orlando";
+
 var queryURL = "http://beermapping.com/webservice/locquery/" + breweryDbApiKey + "/" + userSearch + "&s=json";
  
 // Landing page onclick to take display differnt ROUTE to the ratings and out Home page
-$("#HOME BUTTON CLICK ID FORM TIM").on("click", function() {
+// $("#HOME BUTTON CLICK ID FORM TIM").on("click", function() {
 
   
-});
+// });
 
 
 // Search Button Click to then display the user search
-$("#SEARCH BUTTON CLICK ID FORM TIM").on("click", function() {
+$("#test").on("click", function(event) {
+	event.preventDefault();
+	console.log("CLICK");
 
     $.ajax({
         url: queryURL,
         method: "GET"
       })
       .done(function(response) {
-          
-        var results = response.data;
 
-        console.log(response);
-       
-        // for (var i = 0; i < results.length; i++) {
-        //     // test the return
-        //     console.log(results[i]);
+      	console.log(response)
 
-        //     // display search items in appending to each other based on TIMS code
-        //     var resultsTitle = results[i].
-        // }
+        for (i = 0; i < response.length; i++) {
+        	
+        	// userSearch Brewery Name
+        	console.log("NAME: " + response[i].name);
+
+        	// userSearch Brewery Map return
+        	console.log("BLOGMAP:" + response[i].blogmap);
+
+        	// userSearch Brewery Rating
+        	console.log("OVERALL: " + response[i].overall);
+
+        	// Find More information about this brewery
+        	console.log("REVIEWLINK:" + response[i].reviewlink);
+
+        }
 
       });
 });
