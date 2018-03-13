bb6de3a9b52dc611d81deee15b3bc45b99426d98
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-13 18:51:51 -0400

    all html, api, and post routes working

--- routes/beer-api-routes.js
+++ routes/beer-api-routes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var BreweryDb = require('brewerydb-node');
+var brewdb = new BreweryDb('5e42197c1955ab50b208c667777a5993');
 
 module.exports = function(app) {
   app.get("/api/beer", function(req, res) {
@@ -16,14 +18,18 @@ module.exports = function(app) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
+    // brewdb.beer.getById("avMkil", {}, callback);
+    // callback = JSON.parse(body);
     db.Beer.findOne({
       where: {
         id: req.params.id
       },
       include: [db.Post]
     }).then(function(dbBeer) {
+      // profile = JSON.parse(body);
       res.json(dbBeer);
     });
+       
   });
 
   app.post("/api/beer", function(req, res) {
