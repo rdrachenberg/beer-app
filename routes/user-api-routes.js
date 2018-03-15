85bc3e90519152c51afb3736b06193947e13d43e
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-03-15 14:38:44 -0400

    added logout routes and user routes. Deleted login and signup html

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -61,5 +61,26 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
+  // Route for logging user out
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function (req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    }
+    else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
 
 };
