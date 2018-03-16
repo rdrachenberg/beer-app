175a2ed4787af8e5f2dcaf60fa11d16632cf96e5
Jordan Kue <jkue3000@gmail.com>
2018-03-15 20:53:58 -0400

    BeersJS updated SearchHTML updated

--- public/assets/js/beers.js
+++ public/assets/js/beers.js
@@ -1,5 +1,7 @@
 console.log("LOADED BEERS JS");
 
+glbBrews = [];
+
 // // Search Button Click to then display the user search
 $("#breweryMappingBtn").on("click", function(event) {
 	event.preventDefault();
@@ -21,7 +23,9 @@ $("#breweryMappingBtn").on("click", function(event) {
 
        		console.log(response);
 
+          glbBrews = [];
         	for (i = 0; i < response.length; i++) {
+            glbBrews[i] = response[i];
           	// userSearch Brewery Name
           	console.log("NAME: " + response[i].name);
 
@@ -34,35 +38,34 @@ $("#breweryMappingBtn").on("click", function(event) {
           	// Find More information about this brewery
           	console.log("REVIEWLINK:" + response[i].reviewlink);
           
-          	var btn = "<center><button id=button" + i + ">"+ "<h4>" + response[i].name + "</h4>" + "\n" + "</button></center>";
-          	$("<button>").attr('margin', '10');
+          	var btn = "<center><button type='button' class='btn goldColor' data-number=" + i + " data-toggle='modal' data-target='#breweryInfo' name='"  + response[i].name + "' id=button" + i + ">" + "<h4>" + response[i].name + "</h4>" + "\n" + "</button></center>";
+          	// $("<button>").attr('margin', '10');
           	$("#returnedBreweryInfoBtn").append(btn);
-          	}
 
-          	// for (k = 0; k < response.length; k++){
-          		
-          	// 	$("#button" + k).on("click", function(e){
-          			
-          	// 	})
-          	// }
-        // }
+            // onclick event to display modal with information on the onclick
+            $("#button" + i).on("click", function(e){
+              // clear the modal HTML from the ID
+              $(".modal-body").html('');
+              // console.log($(this).attr('data-number'));
+              
+              // Appending code to modal title
+              console.log(glbBrews[$(this).attr('data-number')].name);
+              $(".modal-title").html("<center><h3>" + glbBrews[$(this).attr('data-number')].name + "</h3></center>");
 
-      });
-	}
+              // appending code to the 
+              $(".modal-body").append("<h4>Address:</h4>");
+              $(".modal-body").append(glbBrews[$(this).attr('data-number')].street + "<br>" + glbBrews[$(this).attr('data-number')].city + ", " + glbBrews[$(this).attr('data-number')].state + " " + glbBrews[$(this).attr('data-number')].zip);
+              $(".modal-body").append("<h4>Phone Number:</h4>" + glbBrews[$(this).attr('data-number')].phone);
+              $(".modal-body").append("<h4>Website: </h4>" +" <a href='http://"+ glbBrews[$(this).attr('data-number')].url + "' target=_blank>" + glbBrews[$(this).attr('data-number')].url + "</a>");
+              $(".modal-body").append("<h4>Review Link: </h4>" +" <a href='http://"+ glbBrews[$(this).attr('data-number')].reviewlink + "' target=_blank>" + glbBrews[$(this).attr('data-number')].reviewlink + "</a>");
+            
+          }); 
+
+      }
+	});
  	
+  }
 });
-
-
-
-
-
-
-
-
-;
-
-//   // var breweryDbURL = "http://api.brewerydb.com/v2/?key=" + breweryDbApi + "/" + userSearch;
-
 
 
 
